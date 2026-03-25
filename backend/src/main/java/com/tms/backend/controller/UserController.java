9b20c4fd4817e0123781eb845d38e5dd2b1a9555
Ashmitha-14 <163744829+Ashmitha-14@users.noreply.github.com>
2026-03-25 12:10:30 +0530

    UserController to TimesheetController

--- backend/src/main/java/com/tms/backend/controller/UserController.java
+++ backend/src/main/java/com/tms/backend/controller/UserController.java
@@ -1,36 +1,39 @@
 package com.timesheet.controller;
 
-import com.timesheet.model.User;
-import com.timesheet.repository.UserRepository;
+import com.timesheet.model.*;
+import com.timesheet.service.TimesheetService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-
 @RestController
-@RequestMapping("/users")
+@RequestMapping("/timesheet")
 @CrossOrigin
-public class UserController {
+public class TimesheetController {
 
     @Autowired
-    private UserRepository userRepo;
+    private TimesheetService service;
 
-    // Create User
-    @PostMapping("/create")
-    public User createUser(@RequestBody User user) {
-        return userRepo.save(user);
+    @PostMapping("/create/{userId}")
+    public Timesheet create(@PathVariable Long userId) {
+        return service.createTimesheet(userId);
     }
 
-    // Get All Users
-    @GetMapping("/")
-    public List<User> getAllUsers() {
-        return userRepo.findAll();
+    @PostMapping("/entry")
+    public TimesheetEntry addEntry(@RequestBody TimesheetEntry entry) {
+        return service.addEntry(entry);
     }
 
-    // Get Users under Manager
-    @GetMapping("/manager/{managerId}")
-    public List<User> getByManager(@PathVariable Long managerId) {
-        return userRepo.findByManagerId(managerId);
+    @PostMapping("/submit/{id}")
+    public Timesheet submit(@PathVariable Long id) {
+        return service.submit(id);
+    }
+
+    @PostMapping("/approve/{id}/{managerId}")
+    public Timesheet approve(
+        @PathVariable Long id,
+        @PathVariable Long managerId) {
+
+        return service.approve(id, managerId);
     }
 }
