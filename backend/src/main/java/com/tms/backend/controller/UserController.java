51d927e7454ce586d2bb8657082eb1e963e4a721
Ashmitha-14 <163744829+Ashmitha-14@users.noreply.github.com>
2026-03-25 12:07:44 +0530

    Implement UserController for user management

---
+++ backend/src/main/java/com/tms/backend/controller/UserController.java
@@ -0,0 +1,36 @@
+package com.timesheet.controller;
+
+import com.timesheet.model.User;
+import com.timesheet.repository.UserRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@CrossOrigin
+public class UserController {
+
+    @Autowired
+    private UserRepository userRepo;
+
+    // Create User
+    @PostMapping("/create")
+    public User createUser(@RequestBody User user) {
+        return userRepo.save(user);
+    }
+
+    // Get All Users
+    @GetMapping("/")
+    public List<User> getAllUsers() {
+        return userRepo.findAll();
+    }
+
+    // Get Users under Manager
+    @GetMapping("/manager/{managerId}")
+    public List<User> getByManager(@PathVariable Long managerId) {
+        return userRepo.findByManagerId(managerId);
+    }
+}
